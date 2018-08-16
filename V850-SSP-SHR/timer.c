8ac7078cba417680024de494608516d5bdf1b244
Kazuhiro Takahashi <takahashi_kazuhiro@nifty.com>
2018-08-16 11:20:57 +0900

    ソースベースライン追加

---
+++ V850-SSP-SHR/timer.c
@@ -0,0 +1,18 @@
+#include "test_serial.h"
+#include "interrupt_table.h"
+#include "chip_timer.h"
+
+static void timer_interrupt_handler(void)
+{
+	printf("timer_interrupt_handler!!\n");
+	target_hrt_handler();
+	return;
+}
+
+void timer_init(void)
+{
+    register_interrupt_handler(22, timer_interrupt_handler);
+    target_hrt_initialize(0);
+    x_enable_int(22);
+}
+
